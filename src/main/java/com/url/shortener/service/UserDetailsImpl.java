7b8840c27dd44f2a16be3ba726f5c1bb5e7928de
vDhinithya <dhinithya12@gmail.com>
2026-03-19 21:07:42 +0530

    modified user service

---
+++ src/main/java/com/url/shortener/service/UserDetailsImpl.java
@@ -0,0 +1,61 @@
+package com.url.shortener.service;
+
+import com.url.shortener.entity.User;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Data
+@NoArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String username;
+    private String email;
+
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserDetailsImpl(String id, String username, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    // this will create our User into UserDetailsImpl Model of spring security
+    public static UserDetailsImpl build(User user) {
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole());
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singletonList(authority)
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+}
